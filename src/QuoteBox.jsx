79aaaf3b8a6ccba2a17d56f4fbd8596ba96d71ab
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2025-01-03 23:11:27 +0100

    feat: change API used to one that is still being maintained

--- src/QuoteBox.jsx
+++ src/QuoteBox.jsx
@@ -1,70 +1,83 @@
 import React from "react";
 import { useState } from "react";
-import { getNewQuote, tweetQuote, buildURL } from "./QuoteBoxFunctions";
-export class QuoteBox extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<div id="quote-box">
-				<QuoteText />
-				<QuoteAuthor />
+
+import "./App.css";
+
+const text = document.querySelector("#text");
+const author = document.querySelector("#author");
+const buildURL = `https://twitter.com/intent/tweet/text=${text} - ${author}`;
+
+function QuoteText() {
+	return <p id="text">Text</p>;
+}
+
+function QuoteAuthor() {
+	return <p id="author">Author</p>;
+}
+
+function NewQuoteButton() {
+	return (
+		<button
+			type="button"
+			id="new-quote"
+			className="btn btn-primary"
+			onClick={getNewQuote}
+		>
+			New Quote
+		</button>
+	);
+}
+
+function TweetButton() {
+	return (
+		<button type="button" className="btn btn-primary">
+			<a
+				id="tweet-quote"
+				target="_blank"
+				rel="noreferrer"
+				onClick={tweetQuote}
+				href={buildURL}
+			>
+				Tweet Quote
+			</a>
+		</button>
+	);
+}
+
+export async function getNewQuote() {
+	let response;
+	const text = document.querySelector("#text");
+	const author = document.querySelector("#author");
+	response = await fetch(
+		"https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand",
+	);
+	const data = await response.json();
+	console.log(data);
+	const q = data[0].content.rendered;
+	const a = data[0].title.rendered;
+	//const { q, a } = data;
+	text.innerHTML = q;
+	author.textContent = a;
+}
+
+function tweetQuote() {
+	const twitterIntent = document.querySelector("#tweet-quote");
+	const text = document.querySelector("#text");
+	const author = document.querySelector("#author");
+	const tweetURL = `https://twitter.com/intent/tweet?text=${text.textContent} - ${author.textContent}`;
+	twitterIntent.setAttribute("href", tweetURL);
+	//	return tweetURL;
+}
+
+export function QuoteBox() {
+	return (
+		<div className="container" id="quote-box">
+			<QuoteText />
+			<QuoteAuthor />
+			<div id="buttons" className="container">
 				<TweetButton />
 				<NewQuoteButton />
 			</div>
-		);
-	}
-}
-
-class QuoteText extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return <p id="text">Text</p>;
-	}
-}
-
-class QuoteAuthor extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return <p id="author">Author</p>;
-	}
-}
-
-class NewQuoteButton extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<button type="button" id="new-quote" onClick={getNewQuote}>
-				New Quote
-			</button>
-		);
-	}
-}
-
-class TweetButton extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<button type="button">
-				<a
-					id="tweet-quote"
-					target="_blank"
-					rel="noreferrer"
-					onClick={tweetQuote}
-					href={buildURL}
-				>
-					Tweet Quote
-				</a>
-			</button>
-		);
-	}
+		</div>
+	);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { QuoteBox } from "./QuoteBox.jsx";
-import { getNewQuote } from "./QuoteBoxFunctions.jsx";
+import { getNewQuote } from "./QuoteBox.jsx";
 
 window.addEventListener("load", () => {
 	getNewQuote();
 });
-
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<QuoteBox />
